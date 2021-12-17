01893e47fcaa15eab2ad52a4ff1911a4a4378408
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2021-12-17 16:35:44 +0900

    W

--- ts_server/utils/isDev.ts
+++ ts_server/utils/isDev.ts
@@ -1,3 +1,4 @@
+// Check what production were in quickly.
 const development: boolean = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
 console.log("Is dev run and found that the current process in in the mode:" + development);
 

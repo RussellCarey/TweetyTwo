0d04931525eb8fec6c562305565679ae179cca2c
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2021-12-17 13:21:18 +0900

    Checking isDev

--- client_two/src/utils/is-dev.ts
+++ client_two/src/utils/is-dev.ts
@@ -1,6 +1,7 @@
 import process from "process";
 
 const development: boolean = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+console.log("Is dev run and found that the current process in in the mode:" + development);
 
 export default function isDev(): boolean {
   return development;

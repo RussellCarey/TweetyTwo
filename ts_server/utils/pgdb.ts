5bed34a87a0d70fc04c4ae3b2e42932fb0ab45dd
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2021-12-19 17:31:15 +0900

    one string for pg

--- ts_server/utils/pgdb.ts
+++ ts_server/utils/pgdb.ts
@@ -1,27 +1,10 @@
 const Pool = require("pg").Pool;
-import isDev from "./isDev";
 
-const getPool = () => {
-  if (!isDev()) {
-    return new Pool({
-      connectionString: process.env.PROD_DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    });
-  }
+const pool = new Pool({
+  connectionString: process.env.PROD_DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
-  if (isDev()) {
-    new Pool({
-      host: "localhost",
-      user: "postgres",
-      password: process.env.DEV_DATABASE_PW,
-      port: 6543,
-      max: 20,
-      idleTimeoutMillis: 30000,
-      connectionTimeoutMillis: 2000,
-    });
-  }
-};
-
-module.exports = getPool();
+module.exports = pool;

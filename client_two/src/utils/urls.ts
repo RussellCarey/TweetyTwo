bf8f489d018da9eacf81c01d5834bb294b7b93eb
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2021-12-20 13:06:28 +0900

    Fixing urls still

--- client_two/src/utils/urls.ts
+++ client_two/src/utils/urls.ts
@@ -1,7 +1,7 @@
 export const projectURLS = {
   production: "https://www.russell-carey.com",
   productionWithAPI: "/tweetyapi",
-  productionLogin: "https://www.russell-carey.com/tweetyapi",
+  productionLogin: "https://russell-carey.com/tweetyapi/api/auth/twitter",
   development: "http://127.0.0.1:3333",
   serverPort: 3333,
   clientPort: 3000,

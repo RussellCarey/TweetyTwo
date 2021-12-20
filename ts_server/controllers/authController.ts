f826fc172e0a390cc4da5dfd54373c10a72bf94f
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2021-12-20 14:15:45 +0900

    W

--- ts_server/controllers/authController.ts
+++ ts_server/controllers/authController.ts
@@ -6,7 +6,7 @@ const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/AppError");
 import isDev from "../utils/isDev";
 
-// On when login is succsesful!!!
+// On when login is succsesful!!!!!
 exports.onTwitterCallback = catchAsync(async (req: IRequestWithUser, res: Response, next: NextFunction) => {
   // Check we were able to get a user through login and save.. :)
   if (!req.user) return next(new AppError("Couldnt verify user", 400));

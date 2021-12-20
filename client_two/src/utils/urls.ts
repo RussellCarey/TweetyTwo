ff24ddfc700d66e4dc5d035c63e568f829293c68
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2021-12-20 13:47:14 +0900

    Fixed nginx and loinks

--- client_two/src/utils/urls.ts
+++ client_two/src/utils/urls.ts
@@ -1,7 +1,7 @@
 export const projectURLS = {
   production: "https://www.russell-carey.com",
   productionWithAPI: "/tweetyapi",
-  productionLogin: "https://russell-carey.com/tweetyapi/api/auth/twitter",
+  productionLogin: "https://russell-carey.com/tweetyapi/auth/twitter",
   development: "http://127.0.0.1:3333",
   serverPort: 3333,
   clientPort: 3000,

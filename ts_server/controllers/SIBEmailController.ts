9663542c4d98216f8c9c290b37ce6e763ad7a9d5
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2021-12-23 16:31:13 +0900

    tEST EMAIL

--- ts_server/controllers/SIBEmailController.ts
+++ ts_server/controllers/SIBEmailController.ts
@@ -10,20 +10,20 @@ apiKey.apiKey = process.env.SENDINBLUE_KEY;
 const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
 
-export const setWelcomeEmail = async (username: string) => {
-  const html = await pug.renderFile("../templates/testEmail.pug", { username: username });
-  return html;
-};
+// export const setWelcomeEmail = async (username: string) => {
+//   const html = await pug.renderFile(`${__dirname}/../templates/testEmail.pug`, { username: username });
+//   return html;
+// };
 
-export const setFailedMessage = async (username: string, message: string, date: string) => {
-  const html = await pug.renderFile("../templates/testEmail.pug", { username: username });
-  return html;
-};
+// export const setFailedMessage = async (username: string, message: string, date: string) => {
+//   const html = await pug.renderFile(`${__dirname}/../templates/testEmail.pug`, { username: username });
+//   return html;
+// };
 
-export const setEmailFullContent = async (subject: string, html: string) => {
-  sendSmtpEmail.subject = subject;
-  sendSmtpEmail.htmlContent = html;
-};
+// export const setEmailFullContent = async (subject: string, html: string) => {
+//   sendSmtpEmail.subject = subject;
+//   sendSmtpEmail.textContent = html;
+// };
 
 export const sendEmail = async (
   type: EEmailType,
@@ -36,14 +36,16 @@ export const sendEmail = async (
   sendSmtpEmail.subject = subject;
   sendSmtpEmail.sender = { name: "Twetty Admin", email: "tweety@russell-carey.com" };
   sendSmtpEmail.to = [{ email: to, name: username }];
+  sendSmtpEmail.subject = subject;
+  sendSmtpEmail.textContent = "THIS IS A TEST EMAIL";
 
-  if (type === EEmailType.isWelcome) {
-    sendSmtpEmail.htmlContent = await setWelcomeEmail(username);
-  }
+  //   if (type === EEmailType.isWelcome) {
+  //     sendSmtpEmail.htmlContent = await setWelcomeEmail(username);
+  //   }
 
-  if (type === EEmailType.isFailed) {
-    sendSmtpEmail.htmlContent = await setFailedMessage(username, message, date);
-  }
+  //   if (type === EEmailType.isFailed) {
+  //     sendSmtpEmail.htmlContent = await setFailedMessage(username, message, date);
+  //   }
 
   await apiInstance.sendTransacEmail(sendSmtpEmail).then(
     function (data: Object) {

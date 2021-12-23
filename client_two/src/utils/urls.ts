8a6a63ef503546b6ad2c0ce37a8748fb3a0f1a05
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2021-12-23 15:48:23 +0900

    Email testing

--- client_two/src/utils/urls.ts
+++ client_two/src/utils/urls.ts
@@ -1,5 +1,6 @@
 export const projectURLS = {
   production: "https://tweety.russell-carey.com",
+  productionWithAPI: "/tweetyapi",
   productionLogin: "https://tweety.russell-carey.com/tweetyapi/auth/twitter",
   development: "http://127.0.0.1:3333",
 };

b34c54e71b699fecbfa2b92f5b3932c7c3be09c7
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2022-06-06 17:33:26 +0900

    removed console logs

--- client_two/src/components/create-window/index.tsx
+++ client_two/src/components/create-window/index.tsx
@@ -1,154 +1,141 @@
-import React, { FunctionComponent, useState, useRef, useContext } from "react";
-import styled from "styled-components";
-import { theme } from "../../styles/theme/theme";
+import React, { FunctionComponent, useState, useRef, useContext } from 'react';
+import styled from 'styled-components';
+import { theme } from '../../styles/theme/theme';
 
-import { useSpring, animated } from "react-spring";
+import { useSpring, animated } from 'react-spring';
 
-import ModalContext from "../../context/modal/ModalContext";
+import ModalContext from '../../context/modal/ModalContext';
 
-import TextArea from "./textbox";
-import InputsContainer from "./inputs-container";
-import DateTimeInput from "./input";
-import CreateButtons from "./button";
+import TextArea from './textbox';
+import InputsContainer from './inputs-container';
+import DateTimeInput from './input';
+import CreateButtons from './button';
 
-import { ERequestOutcomes } from "../../types/errors";
-import { EModal } from "../../context/modal/types";
-import { uploadImageFile, uploadTweet } from "./services/dbServices";
+import { ERequestOutcomes } from '../../types/errors';
+import { EModal } from '../../context/modal/types';
+import { uploadImageFile, uploadTweet } from './services/dbServices';
 
-import { checkDateInputs, checkFileSize, checkWordCount } from "./utils/check-inputs";
+import { checkDateInputs, checkFileSize, checkWordCount } from './utils/check-inputs';
 
-import JobsContext from "../../context/jobs/JobsContext";
-import { convertTimeDateToUnix } from "./utils/convert-to-unix";
+import JobsContext from '../../context/jobs/JobsContext';
+import { convertTimeDateToUnix } from './utils/convert-to-unix';
 
 const Container = styled(animated.div)`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  z-index: 500;
+	position: absolute;
+	bottom: 0;
+	left: 0;
+	z-index: 500;
 
-  width: 100vw;
-  height: 170px;
-  padding: ${theme.spacing.space.large} ${theme.spacing.space.xxxlage};
+	width: 100vw;
+	height: 170px;
+	padding: ${theme.spacing.space.large} ${theme.spacing.space.xxxlage};
 
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
 
-  background-color: ${theme.colors.ui.main};
-  box-shadow: 0 -20px 30px rgba(0, 0, 0, 0.2);
+	background-color: ${theme.colors.ui.main};
+	box-shadow: 0 -20px 30px rgba(0, 0, 0, 0.2);
 
-  @media (max-width: 550px) {
-    height: 300px;
-    flex-direction: column;
-    padding: ${theme.spacing.space.small} ${theme.spacing.space.large};
-    bottom: 50px;
-  }
+	@media (max-width: 550px) {
+		height: 300px;
+		flex-direction: column;
+		padding: ${theme.spacing.space.small} ${theme.spacing.space.large};
+		bottom: 50px;
+	}
 `;
 
 const FileInput = styled.input`
-  display: none;
-  opacity: 100%;
+	display: none;
+	opacity: 100%;
 `;
 
 const CreateWindow: FunctionComponent = () => {
-  const modalContext = useContext(ModalContext);
-  const { showModal } = modalContext;
-  const jobsContext = useContext(JobsContext);
-  const { closeCreateWindow, addNewJob } = jobsContext;
-  const imageInputElement = useRef() as React.MutableRefObject<HTMLInputElement>;
-  const [file, setFile] = useState<File | null>();
-  const [tweet, setTweet] = useState({
-    message: "",
-    date: "",
-    time: "",
-    unix: 0,
-  });
+	const modalContext = useContext(ModalContext);
+	const { showModal } = modalContext;
+	const jobsContext = useContext(JobsContext);
+	const { closeCreateWindow, addNewJob } = jobsContext;
+	const imageInputElement = useRef() as React.MutableRefObject<HTMLInputElement>;
+	const [file, setFile] = useState<File | null>();
+	const [tweet, setTweet] = useState({
+		message: '',
+		date: '',
+		time: '',
+		unix: 0,
+	});
 
-  const animationProps = useSpring({
-    from: { transform: "translateY(400px)", opacity: 0 },
-    to: { transform: "translateY(0px)", opacity: 1 },
-    delay: 100,
-  });
+	const animationProps = useSpring({
+		from: { transform: 'translateY(400px)', opacity: 0 },
+		to: { transform: 'translateY(0px)', opacity: 1 },
+		delay: 100,
+	});
 
-  const dataChangeHandler = (e: React.ChangeEvent) => {
-    const target = e.target as HTMLInputElement;
-    const variable = target.id!;
+	const dataChangeHandler = (e: React.ChangeEvent) => {
+		const target = e.target as HTMLInputElement;
+		const variable = target.id!;
 
-    // Ensure we can use the changed time now not after refresh..
-    const unix = convertTimeDateToUnix(
-      target.id === "date" ? target.value : tweet.date,
-      target.id === "time" ? target.value : tweet.time
-    );
+		// Ensure we can use the changed time now not after refresh..
+		const unix = convertTimeDateToUnix(target.id === 'date' ? target.value : tweet.date, target.id === 'time' ? target.value : tweet.time);
 
-    setTweet({ ...tweet, [variable]: target.value, unix: unix });
-  };
+		setTweet({ ...tweet, [variable]: target.value, unix: unix });
+	};
 
-  const imageOnClickHandler = () => {
-    imageInputElement.current.click();
-  };
+	const imageOnClickHandler = () => {
+		imageInputElement.current.click();
+	};
 
-  const imageOnChangeHandler = (e: React.ChangeEvent) => {
-    const target = e.target as HTMLInputElement;
+	const imageOnChangeHandler = (e: React.ChangeEvent) => {
+		const target = e.target as HTMLInputElement;
 
-    // Check file size is under 10mb to give warning (not allowed on server)
-    const checkFile = checkFileSize(target.files![0]);
-    if (checkFile === ERequestOutcomes.isEmpty) showModal("Image size limit is 10MB", EModal.hasError);
+		// Check file size is under 10mb to give warning (not allowed on server)
+		const checkFile = checkFileSize(target.files![0]);
+		if (checkFile === ERequestOutcomes.isEmpty) showModal('Image size limit is 10MB', EModal.hasError);
 
-    setFile(target.files![0]);
-  };
+		setFile(target.files![0]);
+	};
 
-  const uploadImage = async () => {
-    if (!file) return ERequestOutcomes.isEmpty;
-    const uploadImageReq = await uploadImageFile(file);
-    if (uploadImageReq.hasError) return ERequestOutcomes.hasError;
-    return uploadImageReq;
-  };
+	const uploadImage = async () => {
+		if (!file) return ERequestOutcomes.isEmpty;
+		const uploadImageReq = await uploadImageFile(file);
+		if (uploadImageReq.hasError) return ERequestOutcomes.hasError;
+		return uploadImageReq;
+	};
 
-  //! Need to add modal and resposnes from modal..
-  const handleSubmitClick = async () => {
-    if (!checkWordCount(tweet.message.length)) return showModal("Please check your word count", EModal.hasError);
-    if (!checkDateInputs(tweet)) return showModal("Please check / redo your date or time", EModal.hasError);
+	//! Need to add modal and resposnes from modal..
+	const handleSubmitClick = async () => {
+		if (!checkWordCount(tweet.message.length)) return showModal('Please check your word count', EModal.hasError);
+		if (!checkDateInputs(tweet)) return showModal('Please check / redo your date or time', EModal.hasError);
 
-    const uploadImageAttempt = await uploadImage();
-    if (uploadImageAttempt === ERequestOutcomes.hasError)
-      return showModal("Error creating tweety. Please try again.", EModal.hasError);
-    if (uploadImageAttempt === ERequestOutcomes.hasLimitError)
-      return showModal("Reached upload limit. Please try again in one hour.", EModal.hasError);
+		const uploadImageAttempt = await uploadImage();
+		if (uploadImageAttempt === ERequestOutcomes.hasError) return showModal('Error creating tweety. Please try again.', EModal.hasError);
+		if (uploadImageAttempt === ERequestOutcomes.hasLimitError)
+			return showModal('Reached upload limit. Please try again in one hour.', EModal.hasError);
 
-    const submittedTweet = await uploadTweet(tweet, uploadImageAttempt.data);
-    if (submittedTweet === ERequestOutcomes.hasError)
-      return showModal("Error creating tweet. Please try again.", EModal.hasError);
-    if (submittedTweet === ERequestOutcomes.hasLimitError)
-      return showModal("Reached upload limit. Please try again in one hour.", EModal.hasError);
+		const submittedTweet = await uploadTweet(tweet, uploadImageAttempt.data);
+		if (submittedTweet === ERequestOutcomes.hasError) return showModal('Error creating tweet. Please try again.', EModal.hasError);
+		if (submittedTweet === ERequestOutcomes.hasLimitError) return showModal('Reached upload limit. Please try again in one hour.', EModal.hasError);
 
-    showModal("Tweet created!", EModal.isOK);
+		showModal('Tweet created!', EModal.isOK);
 
-    // Need to implement a queue to give each modals message time to shine..
-    setTimeout(() => {
-      closeCreateWindow();
-      addNewJob(submittedTweet.data.jobs.rows);
-    }, 3000);
-  };
+		// Need to implement a queue to give each modals message time to shine..
+		setTimeout(() => {
+			closeCreateWindow();
+			addNewJob(submittedTweet.data.jobs.rows);
+		}, 3000);
+	};
 
-  return (
-    <Container style={animationProps}>
-      {console.log(tweet)}
-      <TextArea value={tweet.message} onChange={dataChangeHandler} id="message" />
-      <InputsContainer>
-        <DateTimeInput type={"date"} value={tweet.date} onChange={dataChangeHandler} id="date" />
-        <DateTimeInput type={"time"} value={tweet.time} onChange={dataChangeHandler} id="time" />
-        <CreateButtons text={file ? file.name : "add image"} onClick={imageOnClickHandler} />
-        <CreateButtons text={"submit"} onClick={handleSubmitClick} />
-        <FileInput
-          id="file-select"
-          type="file"
-          accept="image/*"
-          ref={imageInputElement}
-          onChange={imageOnChangeHandler}
-        />
-      </InputsContainer>
-    </Container>
-  );
+	return (
+		<Container style={animationProps}>
+			<TextArea value={tweet.message} onChange={dataChangeHandler} id="message" />
+			<InputsContainer>
+				<DateTimeInput type={'date'} value={tweet.date} onChange={dataChangeHandler} id="date" />
+				<DateTimeInput type={'time'} value={tweet.time} onChange={dataChangeHandler} id="time" />
+				<CreateButtons text={file ? file.name : 'add image'} onClick={imageOnClickHandler} />
+				<CreateButtons text={'submit'} onClick={handleSubmitClick} />
+				<FileInput id="file-select" type="file" accept="image/*" ref={imageInputElement} onChange={imageOnChangeHandler} />
+			</InputsContainer>
+		</Container>
+	);
 };
 
 export default CreateWindow;
--- client_two/src/components/create-window/utils/convert-to-unix.ts
+++ client_two/src/components/create-window/utils/convert-to-unix.ts
@@ -1,25 +1,20 @@
-import DateConverter from "date-and-time";
+import DateConverter from 'date-and-time';
 
 export const convertTimeDateToUnix = (date: string, time: string) => {
-  if (date && time) {
-    const dateObj = new Date(date);
+	if (date && time) {
+		const dateObj = new Date(date);
 
-    // Add 0 to days / months that are less than 10..
-    const month = dateObj.getMonth() < 10 ? `0${dateObj.getMonth() + 1}` : `${dateObj.getMonth()}`;
-    const day = dateObj.getDate() < 10 ? `0${dateObj.getDate()}` : `${dateObj.getDate()}`;
-    console.log(month, day);
+		// Add 0 to days / months that are less than 10..
+		const month = dateObj.getMonth() < 10 ? `0${dateObj.getMonth() + 1}` : `${dateObj.getMonth()}`;
+		const day = dateObj.getDate() < 10 ? `0${dateObj.getDate()}` : `${dateObj.getDate()}`;
 
-    // had difficulty with date trasforming so used this npm package. (maybe too many steps) (refactor later)..
-    const convertedDate = DateConverter.parse(
-      `${dateObj.getFullYear()}/${month}/${day} ${time}:00`,
-      "YYYY/MM/DD HH:mm:ss"
-    );
+		// had difficulty with date trasforming so used this npm package. (maybe too many steps) (refactor later)..
+		const convertedDate = DateConverter.parse(`${dateObj.getFullYear()}/${month}/${day} ${time}:00`, 'YYYY/MM/DD HH:mm:ss');
 
-    // Convert to unix time for the database..
-    const unixTime = new Date(convertedDate).getTime();
-    console.log(unixTime);
-    return unixTime;
-  } else {
-    return 0;
-  }
+		// Convert to unix time for the database..
+		const unixTime = new Date(convertedDate).getTime();
+		return unixTime;
+	} else {
+		return 0;
+	}
 };

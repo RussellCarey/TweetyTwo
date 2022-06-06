14870178217c9a4eca1ff4653a0723b9b4b0e02c
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2022-06-06 17:12:53 +0900

    Port change

--- client_two/src/components/modal/index.tsx
+++ client_two/src/components/modal/index.tsx
@@ -1,14 +1,15 @@
-import { FunctionComponent, useContext, useEffect } from "react";
-import styled from "styled-components";
-import { theme } from "../../styles/theme/theme";
+import { FunctionComponent, useContext, useEffect } from 'react';
+import styled from 'styled-components';
+import { theme } from '../../styles/theme/theme';
 
-import { EModal } from "../../context/modal/types";
-import ModalContext from "../../context/modal/ModalContext";
+import { EModal } from '../../context/modal/types';
+import ModalContext from '../../context/modal/ModalContext';
 
 interface ICompProps {
-  type: EModal;
+	type: EModal;
 }
 
+///
 const Container = styled.div`
   width: max-content;
   height: 60px !important;
@@ -29,21 +30,20 @@ const Container = styled.div`
   color: ${theme.colors.text.white};
   padding: ${theme.spacing.space.medium};
 
-  background-color: ${(props: ICompProps) =>
-    props.type === EModal.hasError ? theme.colors.modal.error : theme.colors.modal.ok};
+  background-color: ${(props: ICompProps) => (props.type === EModal.hasError ? theme.colors.modal.error : theme.colors.modal.ok)};
 `;
 
 const Modal: FunctionComponent = () => {
-  const modalContext = useContext(ModalContext);
-  const { modalState, hideModal } = modalContext;
+	const modalContext = useContext(ModalContext);
+	const { modalState, hideModal } = modalContext;
 
-  useEffect(() => {
-    setTimeout(() => {
-      hideModal();
-    }, 3000);
-  }, []);
+	useEffect(() => {
+		setTimeout(() => {
+			hideModal();
+		}, 3000);
+	}, []);
 
-  return <Container type={modalState.type}>{modalState.text}</Container>;
+	return <Container type={modalState.type}>{modalState.text}</Container>;
 };
 
 export default Modal;
